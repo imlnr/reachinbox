30b5d279e7c2f45e343b564194173c83222ed54c
imlnr <imlnr2002@gmail.com>
2024-04-01 12:30:27 +0530

    final commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import Head from '../components/Head';
 import Foot from '../components/Foot';
-import google from "../assets/Frame.png";
+import { Box, Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
-import '../style/login.css';
-import { Box, Button } from '@mui/material';
+import { styled } from '@mui/system';
+import google from "../assets/Frame.png";
 
 const Login = () => {
     return (
-        <Box className=''>
+        <Box>
             <Head />
-            <div className='log-card'>
-                <div className='log-card-inn'>
-                    <div className='log-p-1'>
-                        <p>Create a new account</p>
-                        <Button sx={{
+            <Box className='log-card'>
+                <Box className='log-card-inn'>
+                    <Box className='log-p-1'>
+                        <Typography variant="h6">Create a new account</Typography>
+                        <StyledButton variant="contained" sx={{
                             backgroundColor: 'black',
                             color: 'white',
-                            border: '1px solid #707172',
+                            border: '1px solid white',
                             padding: '8px 16px',
                             width: '90%',
                             borderRadius: '4px',
@@ -25,25 +25,38 @@ const Login = () => {
                         }}>
                             <img src={google} alt="" />
                             <span>Sign Up with Google</span>
-                        </Button>
-                    </div>
-                    <div className='log-p-2'>
-                        <Button sx={{
+                        </StyledButton>
+                    </Box>
+                    <Box className='log-p-2'>
+                        <StyledButton variant="contained" sx={{
                             padding: '13px 35px',
-                            // width: '42.3%',
+                            width: '60%',
                             backgroundImage: 'linear-gradient(to right, #4B63DD, #0524BFFC)',
                             color: 'white',
                             border: 'none',
                             borderRadius: '4px',
                             height: '48px',
-                        }}>Create an Account</Button>
-                        <p>Already have an account? <Link to={'#'}>Sign In</Link></p>
-                    </div>
-                </div>
-            </div>
+                        }}>
+                            Create an Account
+                        </StyledButton>
+                        <Typography variant="body2">Already have an account? <Link to={'#'}>Sign In</Link></Typography>
+                    </Box>
+                </Box>
+            </Box>
             <Foot />
         </Box>
     );
 }
+
+// Styled Button component
+const StyledButton = styled(Button)(({ theme }) => ({
+    padding: '8px 16px',
+    borderRadius: '4px',
+    height: '48px',
+    width: '100%',
+    '& img': {
+        marginRight: theme.spacing(1),
+    },
+}));
 
 export default Login;
